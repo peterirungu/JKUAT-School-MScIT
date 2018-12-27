374bc9f14583af626717ad4547bb1b4242e8e8fb
peterirungu <peterirungu16@gmail.com>
2018-12-27 18:04:18 +0300

    BI

--- MIT3102 Business Intelligence/assignments/zindi_competation.R
+++ MIT3102 Business Intelligence/assignments/zindi_competation.R
@@ -1,15 +1,46 @@
+library(tidyverse)
+library(broom)
+library(ggfortify)
+library(ggplot2)
 
 #Load raw data that is train_revised.csv and test_question.csv
 train <- read.csv("train_revised.csv", header = TRUE)
 test <- read.csv("test_questions.csv", header = TRUE)
+train_aggregate <- read.csv("train_aggregated.csv", header = TRUE)
+final <- read.csv("final.csv", header = TRUE)
 
-#check the datatypes for the train data
-str(train)
+#Fit linear regression model
+#building regression model
+model<-lm(town_from~number_of_tickets, data=final)
+model
 
-# add a column on train dataset to count number of tickets per ride(no_of_tickets)
-train_tickets <- data.frame(no_of_tickets = rep(0, nrow(train)),train[,])
+ 
+model.diag.metrics <- augment(model)
+head(model.diag.metrics)
+ 
+ ggplot(model.diag.metrics, aes(town_from, number_of_tickets)) +
+   geom_point() +
+   stat_smooth(method = lm, se = FALSE) +
+   geom_segment(aes(xend = town_from, yend = .fitted), color = "red", size = 0.3)
 
-#count aggregate for all ride id
+ #Diagnostic plots
+ par(mfrow = c(2, 2))
+ plot(model) 
 
+ autoplot(model) 
 
-table(train$ride_id)
+ 
+ # Add observations indices and
+ # drop some columns (.se.fit, .sigma) for simplification
+ model.diag.metrics <- model.diag.metrics %>%
+   mutate(index = 1:nrow(model.diag.metrics)) %>%
+   select(index, everything(), -.se.fit, -.sigma)
+ # Inspect the data
+ head(model.diag.metrics, 4)
+
+ #linearity of the data
+ plot(model, 1)
+ 
+ #homogenity of the variance
+ plot(model, 3)
+ 
